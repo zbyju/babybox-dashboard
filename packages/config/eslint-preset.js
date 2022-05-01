ec5d46df239e98f3a138a9ccda15a44964d37a46
zbyju <zbynek.jurica@gmail.com>
2022-05-01 12:13:52 +0200

    Remove turborepo default apps, add data-receiver

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,12 +1,3 @@
 module.exports = {
-  extends: ["next", "prettier"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/", "packages/*/"],
-    },
-  },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
+  extends: ["prettier"]
 };
