09ca250ec3b72751f870f3953518532a6399360b
zbyju <zbynek.jurica@gmail.com>
2024-02-15 23:26:38 +0100

    Add context for babyboxes

---
+++ apps/web/components/babyboxes-context.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { babyboxes } from "../data/babyboxes"
+import { createContext } from "react";
+
+export const BabyboxesContext = createContext([] as any[])
+
+export function BabyboxesProvider({ children }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <BabyboxesContext.Provider value={babyboxes.sort((a: any, b: any) => a.slug.localeCompare(b.slug))}>
+      {children}
+    </BabyboxesContext.Provider>
+  )
+}
