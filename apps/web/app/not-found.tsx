bd5a483c13e54a889df76b235065f96773274273
zbyju <zbynek.jurica@gmail.com>
2024-02-15 23:27:09 +0100

    Add go back link

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,10 +1,15 @@
+import GoBack from "@/components/go_back";
+import Navbar from "@/components/navbar";
 import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="w-full h-full min-h-[80vh] flex flex-col justify-center items-center">
-      <h1 className="text-3xl"><span className="font-bold">404</span> - Stránka nebyla nalezena</h1>
-      <p>Pokračujte na hlavní stránku kliknutím <Link href="/" className="underline decoration-1">zde</Link>.</p>
+    <div>
+      <div className="w-full h-full min-h-[80vh] flex flex-col justify-center items-center">
+        <h1 className="text-3xl"><span className="font-bold">404</span> - Stránka nebyla nalezena</h1>
+        <p>Pokračujte na hlavní stránku kliknutím <Link href="/" className="underline decoration-1">zde</Link>.</p>
+        <p>Popřípadě se vraťte o krok zpět kliknutním <GoBack><span className="underline hover:cursor-pointer">zde</span></GoBack>.</p>
+      </div>
     </div>
   )
 }
---
+++ apps/web/components/go_back.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+export default function GoBack({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
+  return <span onClick={() => router.back()}>{children}</span>
+}
