816a3165f2dd40182a8bb10b0e67a215466c001b
zbyju <zbynek.jurica@gmail.com>
2024-03-04 00:52:37 +0100

    Add wrapper for widgets

--- apps/web/components/ui/widget.tsx
+++ apps/web/components/ui/widget.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-
 export default function Widget({
   children,
   title,
+  subtitle,
   className,
   classNameInner,
 }: Readonly<{
   children: React.ReactNode;
   title?: string;
+  subtitle?: string;
   className?: string;
   classNameInner?: string;
 }>) {
   return (
-    <div
-      className={"aspect-[10/7] h-auto w-full min-w-[200px] " + className || ""}
-    >
+    <div className={"h-auto min-w-[200px] " + className || ""}>
       <div
         className={
-          "flex flex-col rounded-lg border border-border px-4 pb-2 pt-4 shadow-md transition-all duration-500 hover:shadow-lg " +
+          "flex flex-col rounded-lg border border-border px-4 pb-3 pt-4 shadow-md transition-all duration-500 hover:shadow-lg " +
             classNameInner || ""
         }
       >
-        <h3 className="mb-3 px-1 text-2xl font-semibold text-foreground">
-          {title || ""}
-        </h3>
+        <div className="mb-3 px-1 py-2">
+          <h3 className="text-2xl font-semibold leading-3 text-foreground">
+            {title || ""}
+          </h3>
+          <h4 className="text-lg text-muted-foreground">{subtitle || ""}</h4>
+        </div>
         <div className="flex-grow overflow-hidden">{children}</div>
       </div>
     </div>
