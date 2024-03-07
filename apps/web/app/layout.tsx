1f615cc8bad869cdcf2f950387013301475d0e7c
zbyju <zbynek.jurica@gmail.com>
2024-03-07 20:38:03 +0100

    Add next-pwa to support PWA

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -33,6 +33,28 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={theme} style={{ colorScheme: theme }}>
+      <head>
+        <meta name="application-name" content="Babybox Dashboard" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Babybox" />
+        <meta
+          name="description"
+          content="Babybox Dashboard aplikace pro sledování a analýzu dat z babyboxů."
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,500,600,700,800,900"
+        />
+      </head>
       <body className={fontSans.className}>
         <div className="mb-12 w-screen max-w-full overflow-x-hidden">
           <AuthProvider>
