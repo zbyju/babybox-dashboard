acd15b4d2ef494b58e691b0e3bdaef418b1fb96b
zbyju <zbynek.jurica@gmail.com>
2024-03-12 16:40:30 +0100

    Improve layout responsivity

--- apps/web/app/app/layout.tsx
+++ apps/web/app/app/layout.tsx
@@ -7,5 +7,9 @@ export default function AppLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
+  return (
+    <ProtectedRoute>
+      <div className="pb-8 lg:pb-2">{children}</div>
+    </ProtectedRoute>
+  );
 }
