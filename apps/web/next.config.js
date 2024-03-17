e6edf9bd4b23013e3cf05081eebbe810fae57ab2
zbyju <zbynek.jurica@gmail.com>
2024-03-17 19:55:18 +0100

    Improve PWA config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  // disable: process.env.NODE_ENV === "development",
-  dynamicStartUrl: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
