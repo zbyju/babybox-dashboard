a4917521c7f36f946f178df4547690cf2f6b73b3
zbyju <zbynek.jurica@gmail.com>
2024-04-04 22:14:33 +0200

    Temporarily remove protected route

--- apps/web/components/protected-route.tsx
+++ apps/web/components/protected-route.tsx
@@ -1,10 +1,3 @@
-import { useAuth } from "./contexts/auth-context";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
-  const router = useRouter();
-
   return children;
 };
