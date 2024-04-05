e92912af2877fb0264e1f3d43e47788e7de57ad6
zbyju <zbynek.jurica@gmail.com>
2024-04-05 17:45:16 +0200

    Add env variables to next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  dest : "public",
+  disable : process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
-  reactStrictMode: true,
+  reactStrictMode : true,
+  env : {
+    NEXT_PUBLIC_URL_SNAPSHOT_HANDLER : process.env
+                                         .NEXT_PUBLIC_URL_SNAPSHOT_HANDLER,
+    NEXT_PUBLIC_URL_BABYBOX_SERVICE : process.env
+                                        .NEXT_PUBLIC_URL_BABYBOX_SERVICE,
+    NEXT_PUBLIC_URL_USER_SERVICE : process.env.NEXT_PUBLIC_URL_USER_SERVICE,
+  }
+
 });
