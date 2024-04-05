0bf585448a40d4baa7fe612b8c34cb17d0e20d48
zbyju <zbynek.jurica@gmail.com>
2024-04-05 17:18:19 +0200

    Add creating default user if none exists

--- apps/user-service/src/db.ts
+++ apps/user-service/src/db.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+
 import logger from "./services/logger";
+import { createUser, findAllUsers } from "./services/user.service";
 
 const connectDB = async () => {
   const dbName = process.env.MONGODB_DATABASE;
@@ -9,6 +11,28 @@ const connectDB = async () => {
   } catch (error) {
     logger.error("Error when connecting to MongoDB: ", error);
     process.exit(1);
+  }
+
+  try {
+    const users = await findAllUsers();
+    if (users.isSome() && users.unwrap.length !== 0) return;
+
+    const defaultUser = process.env.DEFAULT_USERNAME;
+    const defaultPassword = process.env.DEFAULT_PASSWORD;
+    const defaultEmail = process.env.DEFAULT_EMAIL;
+    if (!defaultUser || !defaultPassword || !defaultEmail) {
+      logger.error(
+        `Couldn't create default user: ${defaultUser} ${defaultPassword} ${defaultEmail}`,
+      );
+      return;
+    }
+    await createUser({
+      username: defaultUser,
+      password: defaultPassword,
+      email: defaultEmail,
+    });
+  } catch (err) {
+    logger.error(err);
   }
 };
 
