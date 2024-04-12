6b1b082f0161daa719bbbecc571236ab53fdf369
zbyju <zbynek.jurica@gmail.com>
2024-04-12 19:11:54 +0200

    Add UX icons to textual stats

--- apps/web/components/misc/textual-snapshot-stats.tsx
+++ apps/web/components/misc/textual-snapshot-stats.tsx
@@ -1,7 +1,8 @@
 import { Snapshot } from "@/types/snapshot.types";
 import { calculateAverageSnapshotGap } from "@/utils/stats";
-import { formatDistanceToNow, parse } from "date-fns";
+import { differenceInMinutes, formatDistanceToNow, parse } from "date-fns";
 import { cs } from "date-fns/locale";
+import { BadgeAlert, BadgeCheck, BadgeX } from "lucide-react";
 
 interface Props {
   snapshots: Snapshot[];
@@ -11,18 +12,51 @@ interface Props {
 export default function TextualSnapshotStats(props: Props) {
   if (props.snapshots.length === 0) return null;
   const snapshotsFirst = props.snapshots.slice(0, props.take || 5);
-  const averageGap =
-    calculateAverageSnapshotGap(snapshotsFirst)?.toFixed(0) ?? 0;
-  const lastSnapshotGap = formatDistanceToNow(snapshotsFirst[0].timestamp, {
+  const averageGap = calculateAverageSnapshotGap(snapshotsFirst);
+  const lastSnapshotGapMinutes = Math.abs(
+    differenceInMinutes(new Date(), snapshotsFirst[0].timestamp),
+  );
+  const lastSnapshotGapStr = formatDistanceToNow(snapshotsFirst[0].timestamp, {
     locale: cs,
   });
+
+  const iconsClass = "w-5 h-5";
+  const oldIcon =
+    lastSnapshotGapMinutes > 60 ? (
+      <BadgeX className={iconsClass + " text-destructive"} />
+    ) : lastSnapshotGapMinutes > 10 ? (
+      <BadgeAlert
+        className={iconsClass + " text-orange-600 dark:text-orange-400"}
+      />
+    ) : (
+      <BadgeCheck className={iconsClass + " text-success"} />
+    );
+  const gapIcon =
+    !averageGap || averageGap > 3600 ? (
+      <BadgeX className={iconsClass + " text-destructive"} />
+    ) : averageGap > 700 ? (
+      <BadgeAlert
+        className={iconsClass + " text-orange-600 dark:text-orange-400"}
+      />
+    ) : (
+      <BadgeCheck className={iconsClass + " text-success"} />
+    );
+
   return (
     <div className="flex flex-col leading-5">
       {snapshotsFirst.length > 0 ? (
         <>
           <span>Zobrazeno {snapshotsFirst.length} záznamů.</span>
-          <span>Poslední záznam je {lastSnapshotGap} starý.</span>
-          <span>Průměrná doba mezi příchodem dat je {averageGap} sekund.</span>
+          <span className="inline-flex flex-row flex-wrap items-center gap-1">
+            {oldIcon}
+            Poslední záznam je {lastSnapshotGapStr} starý.
+          </span>
+          <span className="inline-flex flex-row flex-wrap items-center gap-1">
+            {gapIcon}
+            Průměrná doba mezi příchodem dat je {averageGap?.toFixed(2) ??
+              0}{" "}
+            sekund.
+          </span>
         </>
       ) : null}
     </div>
